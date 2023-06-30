642850492ed7024e340a2d605cc79806856a8d67
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2023-06-30 17:52:08 +0000

    Input output of string

---
+++ strings.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class strings{
+        public static void main(String args[]){
+                char arr[]={'a','b'};
+                String str="abcd";
+                String str2=new String("abc");
+                //Input and output in string
+                Scanner s=new Scanner(System.in);
+                String name=s.next();
+                System.out.println(name);
+        }
+}
