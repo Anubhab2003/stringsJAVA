49ef128ee2259035affa509105289b20d3a433b0
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2023-07-01 19:01:34 +0000

    Length of string and concatenation of string

---
+++ length.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class length{
+    public static void main(String args[]){
+        String name="Peter Parker";
+        System.out.println(name.length());
+        //Concatenate
+        String firstname="Tony";
+        String lastname="Stark";
+        String fullname=firstname+ " "+ lastname;
+        System.out.println(fullname);
+    }
+}
