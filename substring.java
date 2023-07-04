57f888c05ecf0489b240ab61b01e5584afbad77f
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2023-07-04 18:19:35 +0000

    To extract substring

---
+++ substring.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class substring{
+    public static String substrings(String str,int si,int ei){
+        String substring=" ";
+        for(int i=si;i<ei;i++){
+            substring+=str.charAt(i);
+
+        }
+        return substring;
+    }
+    public static void main(String args[] ){
+        String str="Hello World";
+        System.out.println(substrings(str,0,3));
+        System.out.println(str.substring(0,5));
+    }
+}
